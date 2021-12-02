530fe7853641d0e05558c69ad7195a25ad7158b7
admin <admin>
2021-12-01 18:53:19 -0600

    Edited file 'webapps/react/backend.py'

--- webapps/react/backend.py
+++ webapps/react/backend.py
@@ -2,7 +2,7 @@ from dataiku.customwebapp import *
 import dataiku
 dataiku.use_plugin_libs("fashion-search")
 from elasticsearch import Elasticsearch
-#from search import FashionSearch
+from search import FashionSearch
 from flask import jsonify, request
 
 # Load Model
@@ -14,23 +14,23 @@ image_folder = dataiku.core.managed_folder.Folder(get_webapp_config()["image_fol
 # Get ES Index Name
 es_index = get_webapp_config()["es_index"] #TODO: Use specified es_index
 
-# #TODO: Use connection based on specified ES index
-# # Initialize Elasticsearch connection
-# es = Elasticsearch()
+#TODO: Use connection based on specified ES index
+# Initialize Elasticsearch connection
+es = Elasticsearch()
 
-# # Initialize Fashion Search main class
-# fs = FashionSearch(es, model_folder, image_folder)
-
-
-# @app.route('/semantic_search', methods=['POST'])
-# def semantic_search():
-#     data = request.json
-#     result = fs.semantic_search(**data)
-#     return jsonify(result)
+# Initialize Fashion Search main class
+fs = FashionSearch(es, model_folder, image_folder)
 
 
-# @app.route('/es_match', methods=['POST'])
-# def es_match():
-#     data = request.json
-#     result = fs.match_query(**data)
-#     return jsonify(result)
+@app.route('/semantic_search', methods=['POST'])
+def semantic_search():
+    data = request.json
+    result = fs.semantic_search(**data)
+    return jsonify(result)
+
+
+@app.route('/es_match', methods=['POST'])
+def es_match():
+    data = request.json
+    result = fs.match_query(**data)
+    return jsonify(result)
